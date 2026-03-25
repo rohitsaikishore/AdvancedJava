bbaaa7dd3b22e9758dc082f95895275019d90c0b
row-hit <rohitsaikishore936@gmail.com>
2026-03-25 16:58:32 +0530

    starting prebasic project to gain spring hands on

---
+++ PrefaceIntroductionProject_@Bean_springStart_here/src/main/java/com/preIntroduction/Parrot.java
@@ -0,0 +1,14 @@
+package com.preIntroduction;
+
+public class Parrot {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
